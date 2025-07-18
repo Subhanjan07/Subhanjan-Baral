c669cd6328a5564f9eabdc090adab286e8226638
Subhanjan Baral <88525846+Subhanjan07@users.noreply.github.com>
2025-07-18 06:24:36 -0400

    Contact Page Updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import clsx from "clsx";
-import FireFliesBackground from "../components/FireFliesBackground";
 
 const inter = Inter({ 
   subsets: ["latin"],
--- src/app/subPages/contact/page.js
+++ src/app/subPages/contact/page.js
@@ -2,12 +2,12 @@ import Image from "next/image";
 import bg from "../../../../public/background/contact-background.png";
 
 
-export default function Contact() {
+export default function Home() {
   return (
     <>
       <Image src={bg} alt="background-image" className="-z-50 fixed top-0 left-0 w-full h-full object-cover object-center opacity-25" /> 
 
-      <article className = "relative w-full flex flex-ceol items-center juustify-center space-y-8">
+      <article className = "relative w-full flex flex-col items-center justify-center space-y-8">
         <div className="flex flex-col items-center justify-center space-y-6 w-3/4">
           <h1 className = "text-accent font-semibold text-center text-4xl capitalize">
             Contact Me
